6f5f9d563f5d8ced0c5793347625a63d4076db7b
Ezya Velmuradov <lena5imova@gmail.com>
2022-08-02 13:27:46 +0300

    final commit

--- hw-react/src/components/Info/info.jsx
+++ hw-react/src/components/Info/info.jsx
@@ -1,10 +1,10 @@
 export const Info = ({users}) => {
     return (
-        <div className="info">
+        <div>
         {users.length > 1 && users.map(item=>(
-            <div>
-            <p>{'Имя: ' + item.name}</p>
-            <p>{'Возраст: ' + item.age}</p>
+            <div className="info">
+            <p className="info_name">{'Имя: ' + item.name}</p>
+            <p>{'Возраст: ' + item.age + ' лет'}</p>
             </div>
             ))}
         </div>
